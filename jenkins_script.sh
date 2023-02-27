424de67368b9079ba46bdbbcf81da6117f0f2914
root <hristovluchy@gmail.com>
2023-02-27 11:33:44 +0200

    Basic script for Jenkins build

--- jenkins_script.sh
+++ jenkins_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "Hi there!"
+echo "Hi there, your build is done!"
+#Build version nummber and date should be available in the output console, as a result of:
 echo Build number $BUILD_NUMBER && echo executed successfully $TAG_DATE
