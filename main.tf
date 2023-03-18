9c79029643084bdddc0d8655b0336b4503774a64
Hris1ov <hristovluchy@gmail.com>
2023-03-18 10:17:55 +0200

    Terraform

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3"{
-    bucket = "devoops1451-terraform-state"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "tf_state_lock"
-    encrypt = true 
- }
 }
 
 # EC2 instance resources
