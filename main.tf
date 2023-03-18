4a69dd1b2d3f5e13a2e44dd659e24e68424706d1
Hris1ov <hristovluchy@gmail.com>
2023-03-18 12:14:21 +0200

    Terraform pipeline

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ provider "aws" {
 }
 
 terraform {
+  backend "s3"{
+    bucket = "devoops1451-terraform-state"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "tf_state_lock"
+    encrypt = true
 }
 
 # EC2 instance resources
